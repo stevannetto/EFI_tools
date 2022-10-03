f3019c84976e130fe66f4a0cfb102ab846956f6f
stevannetto <34402795+stevannetto@users.noreply.github.com>
2022-10-03 17:18:16 +0100

    Update mountEFI.sh

--- mountEFI.sh
+++ mountEFI.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]
     then
     echo "This can only be run as root"
-    exit
+    exit 1
 fi
 
 if [[ ! -d /Volumes/EFI ]]
@@ -10,4 +10,16 @@ if [[ ! -d /Volumes/EFI ]]
         mkdir /Volumes/EFI
 fi
 mount -t msdos /dev/disk0s1 /Volumes/EFI
-echo "EFI partition mounted in /Volumes/EFI/"
+if [ $? -ne 0 ]
+    then
+    echo "Failed to mount EFI partition"
+    exit 1
+else
+    if [[ ! -d /Volumes/EFI/EFI ]]
+    then
+        echo "Partition doesn't look like an EFI system. Unmounting it."
+        diskutil umount /Volumes/EFI
+        exit 1
+    fi
+    echo "EFI partition mounted in /Volumes/EFI/"
+fi
